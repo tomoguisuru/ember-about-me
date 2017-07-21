ad93b5e308dbbb66332d215c36ad4771bc0f875a
Brandon Drake <tomogiusuru@gmail.com>
2017-07-20 23:21:49 -0600

    manifest update

--- config/manifest.js
+++ config/manifest.js
@@ -15,50 +15,52 @@ module.exports = function(/* environment, appConfig */) {
     start_url: "./?utm_source=web_app_manifest",
     icons: [
       {
-        "src": "/android-chrome-36x36.png",
+        "src": "images/icons/android-chrome-36x36.png",
         "sizes": "36x36",
-        "type": "image/png"
       },
       {
-        "src": "/android-chrome-48x48.png",
+        "src": "images/icons/android-chrome-48x48.png",
         "sizes": "48x48",
-        "type": "image/png"
       },
       {
-        "src": "/android-chrome-72x72.png",
+        "src": "images/icons/android-chrome-72x72.png",
         "sizes": "72x72",
-        "type": "image/png"
       },
       {
-        "src": "/android-chrome-96x96.png",
+        "src": "images/icons/android-chrome-96x96.png",
         "sizes": "96x96",
-        "type": "image/png"
       },
       {
-        "src": "/android-chrome-144x144.png",
+        "src": "images/icons/android-chrome-144x144.png",
         "sizes": "144x144",
-        "type": "image/png"
       },
       {
-        "src": "/android-chrome-192x192.png",
+        "src": "images/icons/android-chrome-192x192.png",
         "sizes": "192x192",
-        "type": "image/png"
       },
       {
-        "src": "/android-chrome-256x256.png",
+        "src": "images/icons/android-chrome-256x256.png",
         "sizes": "256x256",
-        "type": "image/png"
       },
       {
-        "src": "/android-chrome-384x384.png",
+        "src": "images/icons/android-chrome-384x384.png",
         "sizes": "384x384",
-        "type": "image/png"
       },
       {
         "src": "images/icons/android-chrome-512x512.png",
         "sizes": "512x512",
-        "type": "image/png"
+      },
+      {
+        "src": "images/icons/favicon-16x16.png",
+        "sizes": "16x16",
+        "targets": ["favicon"]
+      },
+      {
+        "src": "images/icons/favicon-32x32.png",
+        "sizes": "32x32",
+        "targets": ["favicon"]
       }
+
     ],
   };
 };
