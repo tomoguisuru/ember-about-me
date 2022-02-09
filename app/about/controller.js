16c40923dd2d85726192f3834d433f2ca50bba46
Brandon Drake <brandon.drake@edgecast.com>
2022-02-09 10:18:47 -0500

    Update to work history

--- app/about/controller.js
+++ app/about/controller.js
@@ -6,17 +6,18 @@ const AboutController = Controller.extend({
   sortDirection: 'asc',   // [asc, desc]
 
   skills: [
-    { name: 'EmberJS',    level: 5, years: 5 },
+    { name: 'EmberJS',    level: 4, years: 5 },
     { name: 'HTML',       level: 4, years: 10 },
     { name: 'ReactJS',    level: 2, years: 1 },
     { name: 'JavaScript', level: 5, years: 8 },
+    { name: 'TypeScript', level: 3, years: 3 },
     { name: 'SQL',        level: 3, years: 7 },
     { name: 'ASP.NET',    level: 2, years: 5 },
     { name: 'PHP',        level: 2, years: 2 },
     { name: 'Ruby',       level: 2, years: 2 },
     { name: 'Rails',      level: 2, years: 2 },
+    { name: 'Python',     level: 4, years: 3 },
     { name: 'C#',         level: 3, years: 6 },
-    { name: 'Python',     level: 1, years: 1 },
     { name: 'CSS',        level: 4, years: 5 },
     { name: 'Elixir',     level: 1, years: 1 },
     { name: 'Phoenix',    level: 1, years: 1 },
